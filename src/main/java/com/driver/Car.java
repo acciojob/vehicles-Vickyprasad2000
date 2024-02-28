2357d39679bacf9c7c0fcdca1cb3b31d0c44cd0d
Vickyprasad2000 <vickyprasadkn@gmail.com>
2024-02-28 11:05:39 +0530

    vehicle solved

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -13,6 +13,13 @@ public class Car extends Vehicle {
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
         //Hint: Car extends Vehicle
         super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+
     }
     public int getWheels()
     {
@@ -41,7 +48,7 @@ public class Car extends Vehicle {
     }
 
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
--- src/main/java/com/driver/boat.java
+++ src/main/java/com/driver/boat.java
@@ -1,5 +1,21 @@
 package com.driver;
 
+
 public class boat {
-    
+    private String name;
+    private int capacity;
+
+    public boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public String getVehicleName()
+    {
+        return name;
+    }
+
+    public int getVehicleCapacity()
+    {
+        return capacity;
+    }
 }
